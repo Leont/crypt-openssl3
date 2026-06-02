ea99aa0b6ab20650b87ca5efe6f91be3e418592d
Leon Timmermans <fawaka@gmail.com>
2026-06-02 18:22:35 +0200

    Add ASN1 constants

---
+++ lib/Crypt/OpenSSL3/ASN1.pm
@@ -0,0 +1,68 @@
+package Crypt::OpenSSL3::ASN1;
+
+use strict;
+use warnings;
+
+1;
+
+#ABSTRACT: ASN1 constants
+
+=over 41
+
+=item EOC
+
+=item BOOLEAN
+
+=item INTEGER
+
+=item BIT_STRING
+
+=item OCTET_STRING
+
+=item NULL
+
+=item OBJECT
+
+=item OBJECT_DESCRIPTOR
+
+=item EXTERNAL
+
+=item REAL
+
+=item ENUMERATED
+
+=item UTF8STRING
+
+=item SEQUENCE
+
+=item SET
+
+=item NUMERICSTRING
+
+=item PRINTABLESTRING
+
+=item T61STRING
+
+=item TELETEXSTRING
+
+=item VIDEOTEXSTRING
+
+=item IA5STRING
+
+=item UTCTIME
+
+=item GENERALIZEDTIME
+
+=item GRAPHICSTRING
+
+=item ISO64STRING
+
+=item VISIBLESTRING
+
+=item GENERALSTRING
+
+=item UNIVERSALSTRING
+
+=item BMPSTRING
+
+=back
